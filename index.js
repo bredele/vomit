05958524362cc42ee9b51b1786872a305b9902da
bredele <olivier.wietrich@gmail.com>
2016-05-02 17:03:21 -0700

    refactor append array

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 
 module.exports = function(tag, content) {
   var el = document.createElement(tag);
-  if(content) append(el, content);
+  append(el, content);
   return el;
 };
 
 
 function append(el, content) {
-  if(typeof content === 'function') content = content();
-  if(typeof content === 'string') content = document.createTextNode(content);
-  if(content instanceof Array) content = fragment(content);
-  el.appendChild(content);
+  if(content) {
+    if(typeof content === 'function') content = content();
+    if(typeof content === 'string') content = document.createTextNode(content);
+    if(content instanceof Array) content = fragment(content);
+    el.appendChild(content);
+  }
 }
 
 
 function fragment(arr) {
   var frag = document.createDocumentFragment();
   for(var i = 0, l = arr.length; i < l; i++) {
-    var content = arr[i];
-    if(content) append(frag, content);
+    append(frag, arr[i]);
   }
   return frag;
 }
