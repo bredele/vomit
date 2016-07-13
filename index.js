3023153ebebce8a062617b53bb03115059617baf
bredele <olivier.wietrich@gmail.com>
2016-07-13 12:37:48 -0600

    create element with interpolation

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var end = endTag = /^<\/([-A-Za-z0-9_]+)[^>]*>/
  *
  */
 
-module.exports = function(arr) {
-  var str = arr[0]
+module.exports = function(arr, ...args) {
+  var i = 0
+  var str = arr[i]
+  var length = arr.length;
   var match
   var node
   var chars
@@ -18,9 +20,7 @@ module.exports = function(arr) {
       match = str.match(end)
       if(match) {
         str = str.substring(match[0].length)
-        console.log('match end', match)
         match[0].replace(end, ()  => {
-          console.log(arguments)
         })
         chars = false;
       }
@@ -36,8 +36,11 @@ module.exports = function(arr) {
       var index = str.indexOf('<')
       var text = index < 0 ? str : str.substring(0, index)
       str = index < 0 ? '' : str.substring(index)
-      node.innerHTML = text;
-      console.log('text', text, str, index)
+      node.appendChild(document.createTextNode(text))
+    }
+
+    if(!str && ++i != length) {
+      str = args[i - 1] + arr[i]
     }
   }
   return node
