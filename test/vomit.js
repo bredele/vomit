45b4799ec1b6a14a249c8d3699060e6e59141e70
bredele <olivier.wietrich@gmail.com>
2016-07-13 11:28:42 -0600

    return DOM element

--- test/vomit.js
+++ test/vomit.js
@@ -8,5 +8,5 @@ var vomit = require('..')
 tape('should create element', (test) => {
   test.plan(1)
   var btn = vomit`<button></button>`
-  assert.equal(btn.outerHTML, '<button></button>')
+  test.equal(btn.outerHTML, '<button></button>')
 })
