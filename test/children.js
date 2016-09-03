e480c1318e43709111500dbb573335347726a651
bredele <olivier.wietrich@gmail.com>
2016-09-03 13:13:03 -0600

    should interpolate promises

--- test/children.js
+++ test/children.js
@@ -4,6 +4,7 @@
 
 var tape = require('tape')
 var vomit = require('..')
+var promise = require('promises-a');
 
 
 tape('should interpolate string', (test) => {
@@ -20,3 +21,28 @@ tape('should interpolate element', (test) => {
   var section = vomit`<section>${btn}</section>`
   test.equal(section.outerHTML, '<section><button>hello</button></section>')
 })
+
+
+tape('should interpolate promise', (test) => {
+	var value = async('hello world')
+	var btn = vomit`<button>${value}</button>`
+	value.then(() => test.equal(btn.outerHTML, '<button>hello world</button>'))
+})
+
+
+
+/**
+ * Return value after 500ms using promises.
+ * 
+ * @param  {Any} value
+ * @return {Promise}
+ * @api private
+ */
+
+function async(value) {
+  var def = promise()
+  setTimeout(function() {
+	def.resolve(value)
+  }, 5000)
+  return def.promise
+}
